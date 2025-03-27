e9f517792b9dbbfb88b38ece82501068b531bbef
Mohamed Mostafa <mohamedmostafa9557@gmail.com>
2025-03-27 21:49:13 +0200

    updated the database username, password, and name  in `MySQLConnection.connect`, `PostgresSQLConnection` methods

--- SWE/Semester2/Deliverable#2/code/FactoryMethod/MySQLConnection.java
+++ SWE/Semester2/Deliverable#2/code/FactoryMethod/MySQLConnection.java
@@ -7,7 +7,7 @@ public class MySQLConnection implements DatabaseConnection {
     @Override
     public Connection connect() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/dbname", "user", "password");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/HIN", "root", "1234");
         } catch (SQLException e) {
             throw new RuntimeException("Error connecting to MySQL", e);
         }
--- SWE/Semester2/Deliverable#2/code/FactoryMethod/PostgreSQLConnection.java
+++ SWE/Semester2/Deliverable#2/code/FactoryMethod/PostgreSQLConnection.java
@@ -7,7 +7,7 @@ public class PostgreSQLConnection implements DatabaseConnection {
     @Override
     public Connection connect() {
         try {
-            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/dbname", "user", "password");
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/HIN", "root", "1234");
         } catch (SQLException e) {
             throw new RuntimeException("Error connecting to PostgreSQL", e);
         }
